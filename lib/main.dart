9fea09e85a6d6d2de58cbd5a0deaaee2a86465ba
NLevitt-sys <nathan.levitt08@gmail.com>
2026-02-02 16:21:08 +0000

    Nate changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'pages/discovery_page.dart';
 
-void main() {
-  runApp(const MainApp());
-}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clubs App',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const DiscoveryPage(),
     );
   }
 }
