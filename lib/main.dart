e5786e98a906830218a55807df801d18ac7c3057
bigbear23 <clairexu@gmail.com>
2026-02-10 12:02:02 -0500

    main edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'pages/discovery_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'pages/discovery_page.dart';
 import 'pages/login_page.dart';
+import 'pages/main_navigation.dart'; 
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
