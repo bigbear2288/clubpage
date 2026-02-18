aa62952ffcf5d346d9d7e20cd18edd363609fcd6
bigbear23 <clairexu@gmail.com>
2026-02-18 10:25:41 -0500

    changed club cards

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'pages/discovery_page.dart';
+import 'lib/pages/discovery_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
