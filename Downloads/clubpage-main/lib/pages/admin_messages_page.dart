f045dd105411208c6b415ad77a2225883373b5e2
bigbear23 <clairexu@gmail.com>
2026-04-21 11:57:58 -0400

    hi

--- Downloads/clubpage-main/lib/pages/admin_messages_page.dart
+++ Downloads/clubpage-main/lib/pages/admin_messages_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import '../services/message_service.dart';
 import '../services/role_service.dart';
--- Downloads/clubpage-main/main.dart
+++ Downloads/clubpage-main/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
-import 'lib/pages/discovery_page.dart';
 import 'lib/pages/main_navigation.dart';
 import 'lib/pages/login_page.dart';
 
